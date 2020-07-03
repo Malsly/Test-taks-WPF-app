e0415897558c8ec8ac6012cc821b87f8ebd85e0e
Malsly <cma120900@gmail.com>
2020-07-03 20:00:16 +0300

    Added projects to separate layers. Added context with In Memory database. Added entity employee

---
+++ DAL Implementation/WPFAppContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Implementation
+{
+    public class WPFAppContext : DbContext
+    {
+        public WPFAppContext() : base() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "Employees");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        
+        
+    }
+}
