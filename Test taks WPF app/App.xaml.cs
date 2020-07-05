bb39f3b34e42e19e7a05b2fe9ad9effe2f986c71
Malsly <cma120900@gmail.com>
2020-07-05 18:25:16 +0300

    Save commit. Start create views and vm

--- Test taks WPF app/App.xaml.cs
+++ Test taks WPF app/App.xaml.cs
@@ -1,7 +1,4 @@
-﻿using BL.Abstract;
-using BL.Impl;
-using DAL_Implementation;
-using Models;
+﻿using Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -18,15 +15,5 @@ namespace Test_taks_WPF_app
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            IEmployeeService employeeService = new EmployeeService();
-            employeeService.Add(new EmployeeDTO() { Id = 1, Name = "65", Position = "daw", BirthDay = new DateTime(2000, 9, 12) });
-            employeeService.Save();
-            var data = employeeService.GetAll();
-            Console.WriteLine(JsonConvert.SerializeObject(data));
-        }
     }
 }
---
+++ ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,39 @@
+﻿using BL.Abstract;
+using BL.Impl;
+using Entities;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    class EmployeeViewModel
+    {
+        public List<EmployeeDTO> Employees
+        {
+            get;
+            set;
+        }
+
+        public void LoadEmployees()
+        {
+            List<EmployeeDTO> employees = new List<EmployeeDTO>();
+
+            IEmployeeService employeeService = new EmployeeService();
+
+            employeeService.Add(new EmployeeDTO() { Id = 1, Name = "Maks", Position = "Senior", BirthDay = new DateTime(2000, 9, 12) });
+            employeeService.Add(new EmployeeDTO() { Id = 2, Name = "Igor", Position = "Middle", BirthDay = new DateTime(1999, 7, 8) });
+            employeeService.Add(new EmployeeDTO() { Id = 3, Name = "Alex", Position = "Junior", BirthDay = new DateTime(2000, 11, 3) });
+            employeeService.Add(new EmployeeDTO() { Id = 4, Name = "Lera", Position = "Trainee", BirthDay = new DateTime(1998, 1, 7) });
+            
+            employeeService.Save();
+            employees = employeeService.GetAll().Data; 
+
+            Employees = employees;
+        }
+    }
+}
